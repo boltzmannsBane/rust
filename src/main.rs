926d8a7d8c01fe8033463710b69cbd92e79f2d3f
ralmayer <llev4n@gmail.com>
2022-03-03 17:32:18 +0400

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::io;
-
 fn main() {
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("Please enter an array index");
-    let mut index = String::new();
+    let x = plus_one(5);
+    println!("the value of x is: {}", x);
+}
 
-    io::stdin().read_line(&mut index).expect("Failed to read line");
-    let index: usize = index.trim().parse().expect("Index entered wasn't a number");
-    let element = a[index];
-
-    println!("the value of the element at index {} is: {}", index, element);
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
