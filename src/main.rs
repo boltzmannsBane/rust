570c9edac9b06897b89fcc99342e05a3640b9e1c
ralmayer <llev4n@gmail.com>
2022-03-03 16:15:08 +0400

    covered variables, ch ...-ch 3.2

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,13 @@
-use std::cmp::Ordering;
-use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Please enter an array index");
+    let mut index = String::new();
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    io::stdin().read_line(&mut index).expect("Failed to read line");
+    let index: usize = index.trim().parse().expect("Index entered wasn't a number");
+    let element = a[index];
 
-    loop {
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };        
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            println!("You win!");
-            break;
-            }
-        }
-    }
+    println!("the value of the element at index {} is: {}", index, element);
 }
